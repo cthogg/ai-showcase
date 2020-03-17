542f36d6fd21308678c66d9b1e5f89591a31fcd8
cthogg <cthogg@users.noreply.github.com>
2020-03-17 10:46:17 +0000

    feat: add simple list
    adds a simple list with no properties

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApplicationBar from './ApplicationBar';
+import AIList from './AIList';
 
 function App() {
   return (
     <div className="App">
       <ApplicationBar />
+      <AIList />
     </div>
   );
 }
