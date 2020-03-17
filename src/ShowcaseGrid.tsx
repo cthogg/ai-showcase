67741a48fb51e22f7050f2e70c117d704886d290
cthogg <cthogg@users.noreply.github.com>
2020-03-17 15:15:33 +0000

    feat: ensure table is shown only when data
    only when two or more agents are ready for comparison
    is the table shown

--- src/ShowcaseGrid.tsx
+++ src/ShowcaseGrid.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AIList from "./AIList";
 import { FEAgent } from "./types";
 import SelectedAgentsTable from "./SelectedAgentsTable";
-import { mockSelectedAgents } from "./mocks/mockSelectedAgents";
+import { Grid } from "@material-ui/core";
 
 interface ShowCaseGridProps {
   agents: FEAgent[];
@@ -12,26 +12,19 @@ const ShowCaseGrid: React.FunctionComponent<ShowCaseGridProps> = ({
   agents
 }: ShowCaseGridProps): JSX.Element => {
   const [selectedAgents, setSelectedAgents] = useState<FEAgent[]>([]);
-  console.log("selectedAgents", selectedAgents);
-  const selectedAgentsList = () => {
-    if (selectedAgents.length === 0) {
-      return <p> nothing selected </p>;
-    } else {
-      return selectedAgents.map(a => (
-        <React.Fragment key={a.id}>
-          <p> {a.name}</p>
-        </React.Fragment>
-      ));
-    }
-  };
 
   return (
     <>
-      <AIList agents={agents} selectFunction={setSelectedAgents} />
-      {selectedAgentsList()}
-      {6 > 0 && (
-        <SelectedAgentsTable agents={mockSelectedAgents}> </SelectedAgentsTable>
-      )}
+      <Grid container>
+        <Grid item xs={4}>
+          <AIList agents={agents} selectFunction={setSelectedAgents} />
+        </Grid>
+        <Grid item>
+          {selectedAgents.length > 0 && (
+            <SelectedAgentsTable agents={selectedAgents}> </SelectedAgentsTable>
+          )}
+        </Grid>
+      </Grid>
     </>
   );
 };
