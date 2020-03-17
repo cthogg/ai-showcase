1052d0b37890328969a0115a5f6b05b0073e2591
cthogg <cthogg@users.noreply.github.com>
2020-03-17 11:05:40 +0000

    feat: modify frontend types
    modify types to include the categories per the agent

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,26 @@
 // I find separating the backend from frontend types are useful if they are kept in separate repositories
 type AgentId = number;
 
-export interface Agent {
-  readonly id: AgentId;
-  readonly name: string;
-  readonly description: string;
-  readonly tasks: Task[];
+export interface Statistics {
+  averageMean: number;
 }
 
-export interface Task {
+export interface CategoryStatistic {
+  category: "memory" | "planning" | "logic";
+  statistics: Statistics;
+}
+
+export interface FEAgent {
+  id: AgentId;
+  name: string;
+  description: string;
+  tasks: FETask[];
+  statistics: {
+    categories: CategoryStatistic[];
+  };
+}
+
+export interface FETask {
   readonly id: string;
   readonly name: string;
   readonly category: "memory" | "planning" | "logic";
