2593a1409761665a8f2edfee87120874f452a398
cthogg <cthogg@users.noreply.github.com>
2020-03-17 11:51:13 +0000

    feat: create parser to show average from categories

--- src/backendFrontendParser.ts
+++ src/backendFrontendParser.ts
@@ -1,10 +1,45 @@
-import { BEAgent, FEAgent } from "./types";
+import { BEAgent, FEAgent, CategoryStatistic } from "./types";
 
+// list all categories
+// for each of these categories find all of the tasks in it
+// average all of these tasks from the categories
+//
+
+// FIXME: convert cateogry names into enums
+export const findCategoryAverageMeanFromAgent = (
+  agent: BEAgent,
+  category: "memory" | "planning" | "logic"
+): number => {
+  const tasksFromAgentWithCategory = agent.tasks.filter(
+    task => task.category === category
+  );
+  const addArray = (a: number, b: number) => a + b;
+  const taskScores = tasksFromAgentWithCategory.map(t => t.score);
+  const scoresTotal = taskScores.reduce(addArray, 0);
+  const averageScore = scoresTotal / tasksFromAgentWithCategory.length;
+  return averageScore;
+};
+//FIXME: remove ts-ignores
+const allCategories = ["memory", "planning", "logic"];
+const createCategoriesFromAgent = (agent: BEAgent): CategoryStatistic[] => {
+  return allCategories.map(
+    (catName: string): CategoryStatistic => {
+      return {
+        //@ts-ignore
+        category: catName,
+        statistics: {
+          //@ts-ignore
+          averageMean: findCategoryAverageMeanFromAgent(agent, catName)
+        }
+      };
+    }
+  );
+};
 export const beFeAgentParser = (beAgents: BEAgent[]): FEAgent[] => {
   const output = beAgents.map(
     (agent: BEAgent): FEAgent => ({
       statistics: {
-        categories: [{ category: "memory", statistics: { averageMean: 123 } }]
+        categories: createCategoriesFromAgent(agent)
       },
       ...agent
     })
